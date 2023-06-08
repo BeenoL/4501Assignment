31918d8ad8dedce42ec50517261130558bf60e25
BeenoL <rialeungcl@gmail.com>
2023-06-08 19:20:20 +0800

    record

--- app/src/main/java/com/example/a4501assignment/recordControl/record.java
+++ app/src/main/java/com/example/a4501assignment/recordControl/record.java
@@ -6,6 +6,19 @@ public class record {
 
     public record(int moves, String time, String dayTime){
         this.moves = moves;
+        this.daytime =dayTime;
+        this.time = time;
+    }
 
+    public int getMoves(){
+        return moves;
+    }
+
+    public String getTime(){
+        return time;
+    }
+
+    public String getDaytime(){
+        return daytime;
     }
 }
